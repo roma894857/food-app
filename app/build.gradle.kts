fc1b6b7bd422f2f48f18670b84d324545e1737a0
Devanchik <hoholl2@proton.me>
2026-07-22 21:56:19 +0300

    994994

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    id("com.android.application") version "8.4.0"
+    id("org.jetbrains.kotlin.android") version "1.9.22"
     kotlin("plugin.serialization") version "1.9.22"
 }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,13 @@
 // Root project settings
 rootProject.name = "FoodApp"
 
+// Configure repositories for the whole project
+repositories {
+    google()
+    mavenCentral()
+    gradlePluginPortal()
+}
+
 // Include the app module
 subprojects {
     apply plugin: "org.jetbrains.kotlin.android"
